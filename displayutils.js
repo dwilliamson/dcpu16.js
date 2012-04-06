da0d1aa88306763647ad4c12b98de76b9c1d7f86
unknown <Don Williamson@DonWilliamson.(none)>
2012-04-06 18:04:49 +0100

    first commit

---
+++ displayutils.js
@@ -0,0 +1,57 @@
+function HashString(str)
+{
+	var hash = 5381;
+	for (var i = 0; i < str.length; i++)
+	{
+		var c = str.charCodeAt(i);
+		hash = c + (hash << 6) + (hash << 16) - hash;
+	}
+	return hash;
+}
+
+
+// Pads to 6 nibbles
+function dec2hex(i)
+{
+	return (i + 0x1000000).toString(16).substr(-6).toUpperCase();
+}
+
+
+// Simple 24-bit RGB ops
+function lerp8(a, b, t)
+{
+	a &= 0xFF;
+	b &= 0xFF;
+	return Math.floor(a + (b - a) * t);
+}
+function lerprgb(a, b, t)
+{
+	var c = lerp8(a, b, t);
+	c |= lerp8(a >> 8, b >> 8, t) << 8;
+	c |= lerp8(a >> 16, b >> 16, t) << 16;
+	return c;
+}
+
+
+// Adapted from http://www.javascriptkit.com/javatutors/setcss3properties.shtml
+function getsupportedprop(proparray)
+{
+	//reference root element of document
+	var root=document.documentElement;
+
+	//loop through possible properties
+	for (var i = 0; i < proparray.length; i++)
+	{
+		//if the property value is a string (versus undefined)
+		if (typeof root.style[proparray[i]] == "string")
+			return proparray[i];
+	}
+}
+var shadowprop = getsupportedprop(['boxShadow', 'MozBoxShadow', 'WebkitBoxShadow']);
+var roundborderprop = getsupportedprop(['borderRadius', 'MozBorderRadius', 'WebkitBorderRadius']);
+var csstransform = getsupportedprop(['transform', 'MozTransform', 'WebkitTransform', 'msTransform', 'OTransform']) ;
+function changecssproperty(target, prop, value, action)
+{
+	if (typeof prop != "undefined")
+		target.style[prop] = (action == "remove") ? "" : value;
+}
